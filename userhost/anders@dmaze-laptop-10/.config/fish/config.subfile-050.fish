ee1bc2459b7553b3ec2a94379e367c467f1a1159
Anders Østhus <184104+andersosthus@users.noreply.github.com>
2026-08-14 13:47:06 +0200

    added DOTNET env

--- userhost/anders@dmaze-laptop-10/.config/fish/config.subfile-050.fish
+++ userhost/anders@dmaze-laptop-10/.config/fish/config.subfile-050.fish
@@ -1,7 +1,10 @@
 set -gx ANDROID_HOME $HOME/Android/Sdk
 set -gx ANDROID_AVD_HOME $HOME/.config/.android/avd
+set -gx DOTNET_ROOT $HOME/.dotnet
 
 fish_add_path -p ~/fvm/default/bin
 fish_add_path -p $ANDROID_HOME/cmdline-tools/latest/bin
 fish_add_path -p $ANDROID_HOME/platform-tools
+fish_add_path -p $HOME/.dotnet
+fish_add_path -p $HOME/.dotnet/tools
 
